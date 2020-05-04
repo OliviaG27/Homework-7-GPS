f51bba2f3d22f31f80ea593c3bd695e7b7d01ef0
Olivia Gennaro <o.gennaro@setonhill.edu>
2020-05-04 18:02:27 -0400

    Initial Commit

---
+++ Homework 7: GPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework 7: GPS
+//
+//  Created by Olivia Gennaro on 5/4/20.
+//  Copyright © 2020 Olivia Gennaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
